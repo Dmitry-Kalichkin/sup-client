d5e2937770c83cc279dc89b3c11e39a1e612393b
Дмитрий Каличкин <dmitriykalichkin@icloud.com>
2025-02-26 01:25:31 +0700

    F-3177. Add header and disable SSR completly

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+    import { authorized, role } from '$lib/stores/UserStore';
+    import { Role } from "$lib/data/Role";
+
+    let isManager = $derived($authorized && ($role === Role.ADMIN || $role === Role.DEANERY));
+</script>
+
+<header>
+    <nav>
+        <a href="/">
+            <img src="/images/logo.svg" alt="logo" class="logo"/>
+            <p class="title">
+                Система
+                Учета
+                Пропусков
+            </p>
+        </a>
+        {#if isManager}
+            <a href="/skips">Пропуски</a>
+        {/if}
+        {#if isManager}
+            <a href="/users">Пользователи</a>
+        {/if}
+        {#if $authorized}
+            <a href="/skips/my">Мои пропуски</a>
+        {/if}
+    </nav>
+    <nav>
+        {#if $authorized}
+            <a href="/profile">Пользователь</a>
+        {:else}
+            <a href="/login">Вход</a>
+        {/if}
+    </nav>
+</header>
+
+<style>
+    header {
+        padding: 10px;
+        display: flex;
+        justify-content: space-between;
+    }
+
+    nav {
+        display: flex;
+    }
+
+    a {
+        display: flex;
+        align-items: center;
+        text-decoration: none;
+        color: black;
+        margin: 10px;
+    }
+
+    .logo {
+        width: 50px;
+        height: 50px;
+        margin-right: 10px;
+    }
+
+    .title {
+        font-size: 14px;
+        font-weight: bold;
+        display: flex;
+        width: 90px;
+    }
+</style>
